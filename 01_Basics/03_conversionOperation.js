3aef25b3bc3062be3d459269190b254de26c189e
Aditya MIshra <102588012+aditya05200@users.noreply.github.com>
2026-09-01 16:35:34 +0000

    conversion of data type

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = null
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33qbc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn =  "Aditya"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"Aditya" => true
+
+let checkNumber = 33
+
+let numToString = String(checkNumber)
+console.log(numToString);
+
+console.log(typeof numToString)
