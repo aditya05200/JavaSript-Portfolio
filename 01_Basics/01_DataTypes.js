1f7398916b2df881df0cc84898294f4a2f4ea2ac
Aditya MIshra <102588012+aditya05200@users.noreply.github.com>
2026-09-01 11:32:45 +0000

    DataTypes

---
+++ 01_Basics/01_DataTypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat JavaScript code as newer version
+
+// Data Types in JS
+
+// Numeric: Integer eg: 12121, Decimal eg: 3.14, BigInt,
+// Numbers can be written with or without decimals
+
+// String written in '' or  "" 
+
+// Boolean true/false
+
+// null => Standalone value
+
+// undefined 
+
+// Symbol => Unique:  mostly use in React
+
+// Object
+
+// How to check Date type of Value
+
+console.log(typeof "Aditya")
+console.log(typeof 12)
+console.log(typeof true)
+console.log(typeof 3.14)
+
+console.log(typeof 2342535235235235325235235235235325235)
+
+console.log(typeof null) // Output :  Object
+
+console.log(typeof undefined) // Output : Undefined
+console.log(typeof Symbol); // Output : function
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 12412
+let accountEmail = "aditya@gmail.com"
+var accountPassword="124141"
+
+
+accountCity = "Noida"
+
+// accountId = 131
+accountEmail="ad@gmail.com"
+accountPassword="2132321"
+accountCity="Delhi"
+console.table([accountEmail,accountPassword,accountCity])
+
+console.log(accountId);
+
+let name = "Rahul"
+console.log(name)
+console.log(age)
+/*
+Perefer not to use var 
+Because Issue in Block Scope and Functional Scope
+*/
+
