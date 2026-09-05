98b1d9463bb089d8b32bd7fae62b2bfb7b731e22
Aditya MIshra <102588012+aditya05200@users.noreply.github.com>
2026-09-05 10:54:07 +0000

    num_maths

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,26 @@
+// const value = 200
+// console.log(value);
+
+// const num =  new Number(200)
+
+// console.log(num)
+
+// console.log(num.toString());
+// console.log(num.toFixed(2));
+
+// const newNum = 233.8
+// console.log(newNum.toPrecision(1));
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.9));
+console.log(Math.max(4,2,5,8));
+console.log(Math.min(3,2,1,5));
+console.log(Math.random());
+
+
+
