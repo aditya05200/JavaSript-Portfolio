365fd99e71a3e16fce619ae70b3393e448c73fe2
Aditya MIshra <102588012+aditya05200@users.noreply.github.com>
2026-09-17 08:09:52 +0000

    init Date.js

---
+++ 01_Basics/07_Date.js
@@ -0,0 +1,20 @@
+let myDate = new Date()
+//console.log(myDate.toDateString());
+//console.log(myDate.toString());
+//console.log(myDate.toLocaleString());
+// let newDate = new Date(2023,0,23,5,3)
+// console.log(newDate.toString());
+// console.log(newDate.
+// toLocaleString())
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myDate.getTime() );
+//console.log(Math.floor(myTimeStamp/1000));
+
+console.log(myDate.getMonth()+1);
+console.log(myDate.getDay());
+console.log(myDate.toLocaleString('default', {weekday:"long"}));
+
+
+
+
